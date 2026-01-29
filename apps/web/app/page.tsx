c3006c662e548d76b479d5a6aa4fcce26c7f60d4
jonathan <jtvu04@gmail.com>
2026-01-29 12:35:31 -0800

    build nextjs environment

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">we are not really strangers</h1>
+    </main>
+  );
+}
